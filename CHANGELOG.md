edd61c28a1bcc6608bf01dab62d684c1e674ee11
Daniel Indlekofer <daniel@indlekofer.org>
2021-05-05 23:02:32 +0200

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## [Unreleased]
 
+## 1.0.1 (2021-05-05)
+
+* dependencies updated
+
 ## 1.0.0 (2019-04-25)
 
 * init
--- cjs/index.js
+++ cjs/index.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 var _default = function _default(data) {
   return typeof data === 'string' || !!data && _typeof(data) === 'object' && Object.prototype.toString.call(data) === '[object String]';
